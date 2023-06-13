7c1937f93b14bdc28ce8ab9be79b5c5a6cfa4efd
Erika Santos <dossantos.erika08@gmail.com>
2023-06-13 18:52:22 -0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './shared/home/home.component';
+import { LoginComponent } from './shared/login/login.component';
+import { RegisterComponent } from './shared/register/register.component';
+import { ConsultaComponent } from './shared/consulta/consulta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
